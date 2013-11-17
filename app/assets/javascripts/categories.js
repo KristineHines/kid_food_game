db031794def5c0966833736afb542e5e198cfb66
Vanessa Ramos <vanessa@3scale.net>
2013-11-17 00:32:17 -0800

    fixed game button

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
+	var click_count = 0
 	$("#category_button").on("click", function(event){
 		event.preventDefault()
 		var active = $(".active");
-		var last = $('#category_2');
+		click_count = click_count + 1
 		active.removeClass("active").addClass("inactive");
 		active.next().removeClass("inactive").addClass("active");
+		if (click_count == 2){
+			$("#category_button").remove();
+			$(".results_button").removeClass("results_button");
+		}
 	});
 });
+
 
